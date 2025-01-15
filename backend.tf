35d4f9df19b7bcd3430d73cc0db1e4f00cef02f0
shihabnawazi <shihabnawazi@gmail.com>
2025-01-15 20:40:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ajit-inamdar-tech-terraform-backend"
+    bucket         = "shihab-tech-terraform-backend"
     encrypt        = true
     key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "ajit-inamdar-tech-terraform-backend"
+    region         = "us-east-1"   
   }
 }
